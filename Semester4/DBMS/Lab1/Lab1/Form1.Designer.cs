507607ace79b41ffb051946a3dd36ce995b3578d
adrianatim <adriana_timis2017@yahoo.com>
2021-04-03 22:00:05 +0300

    DBMS- Lab1

---
+++ Semester4/DBMS/Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace Baze1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDualControlCar = new System.Windows.Forms.DataGridView();
+            this.dgvDrivingInstructor = new System.Windows.Forms.DataGridView();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDualControlCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrivingInstructor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDualControlCar
+            // 
+            this.dgvDualControlCar.AllowUserToAddRows = false;
+            this.dgvDualControlCar.AllowUserToDeleteRows = false;
+            this.dgvDualControlCar.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dgvDualControlCar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDualControlCar.Location = new System.Drawing.Point(31, 23);
+            this.dgvDualControlCar.Name = "dgvDualControlCar";
+            this.dgvDualControlCar.Size = new System.Drawing.Size(462, 226);
+            this.dgvDualControlCar.TabIndex = 0;
+            // 
+            // dgvDrivingInstructor
+            // 
+            this.dgvDrivingInstructor.AllowUserToAddRows = false;
+            this.dgvDrivingInstructor.AllowUserToDeleteRows = false;
+            this.dgvDrivingInstructor.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dgvDrivingInstructor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDrivingInstructor.GridColor = System.Drawing.Color.DarkGray;
+            this.dgvDrivingInstructor.Location = new System.Drawing.Point(695, 23);
+            this.dgvDrivingInstructor.Name = "dgvDrivingInstructor";
+            this.dgvDrivingInstructor.Size = new System.Drawing.Size(465, 226);
+            this.dgvDrivingInstructor.TabIndex = 1;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Pink;
+            this.deleteButton.Location = new System.Drawing.Point(67, 413);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(95, 48);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.Color.Pink;
+            this.updateButton.Location = new System.Drawing.Point(67, 340);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(95, 51);
+            this.updateButton.TabIndex = 3;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.Pink;
+            this.addButton.Location = new System.Drawing.Point(67, 269);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(95, 50);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.DeepPink;
+            this.connectButton.Location = new System.Drawing.Point(525, 100);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(139, 70);
+            this.connectButton.TabIndex = 6;
+            this.connectButton.Text = "CONNECT";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(525, 285);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(525, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(525, 364);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(525, 406);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(525, 441);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(153, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(558, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Dual control car";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(713, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "dccId";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(713, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "dccBrand";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(713, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "dccFuel";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(713, 409);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "diId";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(713, 448);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "carTypeCategory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1172, 485);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dgvDrivingInstructor);
+            this.Controls.Add(this.dgvDualControlCar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDualControlCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrivingInstructor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDualControlCar;
+        private System.Windows.Forms.DataGridView dgvDrivingInstructor;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Semester4/DBMS/Lab1/Lab1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Baze1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        DataSet ds;
+        SqlCommandBuilder commandBuilder;
+        SqlDataAdapter daDualControlCar, daDrivingInstructor;
+        BindingSource bsDualControlCar, bsDrivingInstructor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source=DESKTOP-PU9UT93\SQLEXPRESS;Initial Catalog=DrivingSchoolDB;Integrated Security=SSPI");
+            initialTable();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var command = new SqlCommand("INSERT INTO dual_control_car VALUES (@dccId, @dccBrand, @dccFuel, @diId, @carTypeCategory)", connection);
+            connection.Open();
+            try
+            {
+                command.Parameters.AddWithValue("@dccId", textBox1.Text);
+                command.Parameters.AddWithValue("@dccBrand", textBox2.Text);
+                command.Parameters.AddWithValue("@dccFuel", textBox3.Text);
+                command.Parameters.AddWithValue("@diId", textBox4.Text);
+                command.Parameters.AddWithValue("@carTypeCategory", textBox5.Text);
+
+                command.ExecuteNonQuery();
+
+                clearTextBoxes();
+                initialTable();
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("The following error occured:" + exc.ToString());
+            }
+            connection.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            var command = new SqlCommand("UPDATE dual_control_car SET dccBrand=@dccBrand, dccFuel=@dccFuel, diId=@diId, carTypeCategory=@carTypeCategory WHERE dccId = @dccId", connection);
+
+            connection.Open();
+            try
+            {
+                command.Parameters.AddWithValue("@dccId", textBox1.Text);
+                command.Parameters.AddWithValue("@dccBrand", textBox2.Text);
+                command.Parameters.AddWithValue("@dccFuel", textBox3.Text);
+                command.Parameters.AddWithValue("@diId", textBox4.Text);
+                command.Parameters.AddWithValue("@carTypeCategory", textBox5.Text);
+
+                command.ExecuteNonQuery();
+
+                clearTextBoxes();
+                initialTable();
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("The following error occured:" + exc.ToString());
+            }
+            connection.Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            var command = new SqlCommand("DELETE FROM dual_control_car WHERE dccId = @dccId", connection);
+            connection.Open();
+            try
+            {
+                command.Parameters.AddWithValue("@dccId", textBox1.Text);
+                command.ExecuteNonQuery();
+
+                clearTextBoxes();
+                initialTable();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("The following error occured:" + exc.ToString());
+            }
+            connection.Close();
+        }
+
+        private void initialTable()
+        {
+            ds = new DataSet();
+            daDualControlCar = new SqlDataAdapter("select * from dual_control_car", connection);
+            daDrivingInstructor = new SqlDataAdapter("select * from driving_instructor", connection);
+
+            commandBuilder = new SqlCommandBuilder(daDrivingInstructor);
+
+            daDualControlCar.Fill(ds, "dual_control_car");
+            daDrivingInstructor.Fill(ds, "driving_instructor");
+
+            DataRelation dr = new DataRelation("FK_DI_DCC", ds.Tables["driving_instructor"].Columns["diId"],
+                ds.Tables["dual_control_car"].Columns["diId"]);
+            ds.Relations.Add(dr);
+
+            bsDualControlCar = new BindingSource();
+            bsDrivingInstructor = new BindingSource();
+
+            bsDrivingInstructor.DataSource = ds;
+            bsDrivingInstructor.DataMember = "driving_instructor";
+
+            bsDualControlCar.DataSource = bsDrivingInstructor;
+            bsDualControlCar.DataMember = "FK_DI_DCC";
+
+            dgvDualControlCar.DataSource = bsDualControlCar;
+            dgvDrivingInstructor.DataSource = bsDrivingInstructor;
+        }
+
+        private void clearTextBoxes()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+    }
+}
